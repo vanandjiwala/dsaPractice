8ac932f5622982e641b7a6c33ae69b3dbfc77b77
vasav.anandjiwala <vasav_2035@csu.fullerton.edu>
2016-09-24 14:07:02 -0400

    Ex10-PelindromeIndex2

--- src/com/HRank/String/Ex10.java
+++ src/com/HRank/String/Ex10.java
@@ -3,40 +3,69 @@ package com.HRank.String;
 import java.util.Scanner;
 
 public class Ex10 {
+	
+	
+//	https://www.hackerrank.com/challenges/palindrome-index
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int t = scan.nextInt();
 		StringBuilder build = new StringBuilder();
-		StringBuilder reverseBuild = new StringBuilder();
+		StringBuilder temp = new StringBuilder();
 		int offset = 0;
+		int n = 0;
+		int result[] = new int[t];
 		
 		for(int i = 0; i<t; i++){
 			build.append(scan.next());
-			reverseBuild.append(build.reverse().toString());
-			if(build.toString().equals(reverseBuild.toString())){
-				System.out.println("-1");
+			temp.append(build.toString());
+			n = build.length()/2;
+			//System.out.println(build);
+			if(build.toString().equals(build.reverse().toString())){
+				result[i] = -1;
 			}else{
 				build.reverse();
-				offset = build.length()/2;
-				for(int j = 0; j<offset; j++){
+				offset = build.length() - 1;
+				//System.out.println("offset is " + offset);
+				for(int j = 0; j<n; j++){
 					
-					if(build.charAt(j) != build.charAt(j)){
-						
-						System.out.println(build.toString().substring(0, i));
+					if(build.charAt(j) != build.charAt(offset - j)){
+
+						if((temp.deleteCharAt(j).toString()).equals(temp.reverse().toString())){
+							
+							build.deleteCharAt(j);
+							//System.out.println(j);
+							result[i] = j;
+							break;
+						}else{
+							
+							build.deleteCharAt(offset - j);
+							//System.out.println(offset - j);
+							result[i] = offset - j;
+							break;
+						}
 						
 						
 					}
 					
-					System.out.println(j);
+					
 				}
 				
 				
 			}
 			
+			build.delete(0, build.length());
+			temp.delete(0, temp.length());
 			
 		}
+		
+		for(int i=0;i < result.length; i++){
+			System.out.println(result[i]);
+		}
+		
+		scan.close();
+		
 	}
 
 }
